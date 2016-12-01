aaf0a046768b6cf90c808fe183440462a2929a1f
Avital Oliver <avital@theWE.net>
2016-12-01 15:34:57 -0800

    WIP/maybe? better alpha control

--- visualize_a3c.py
+++ visualize_a3c.py
@@ -38,37 +38,45 @@ def array_from_figure(fig):
     width, height = fig.canvas.get_width_height()
     return data.reshape((height, width, 4))
 
-#metadata = metadata[0:4]
+#metadata = metadata[0:30]
 for index in range(2, len(metadata)):
     print('{0}/{1}'.format(index+1, len(metadata)))
-    history_size = 1
-    x = np.arange(max(0, index-history_size), index)
+    history_size = 10
+    x = np.arange(max(0, index-history_size), min(len(metadata)-1, index+history_size))
 
-    def plot_rewards():
-        rewards = [metadata[other_index]['scalars']['reward'] for other_index in x]
-        plt.subplot(3, 1, 1)
-        plt.plot(x, rewards)
-        plt.axis([x[0], x[-1], 0, 4])
-#        plt.xlim(x[0], x[-1])
-        plt.ylabel('reward')
+    def plot_scalar(prop, color_func, label, val_min, val_max):
+        scalars = [metadata[other_index]['scalars'][prop] for other_index in x]
+        y_pos = 1
+
+        fig = plt.figure(figsize=(1.5, 1), dpi=50)
+        fig.patch.set_facecolor('none')
+        plt.subplot(1, 1, 1, axisbg='none')
+
+        cropped_rel = metadata[index]['scalars'][prop]
+        cropped_rel = (cropped_rel - val_min) / (val_max - val_min)
+        cropped_rel = max(cropped_rel, 0)
+        cropped_rel = min(cropped_rel, 1)
+        print(prop, cropped_rel)
+
+        plt.plot(x, scalars, alpha=0.3, color='white')
+        plt.axis('off')
+        plt.ylim(val_min, val_max)
+        plt.xlim(index-history_size, index+history_size)
+        plt.axvline(index, color='white')
+        plt.axhline(0, color='white', linestyle='dotted')
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+#        plt.ylabel(label)
         ax = plt.gca()
         ax.set_autoscale_on(False)
-
-    def plot_value_predictions():
-        value_predictions = [metadata[other_index]['scalars']['value'] for other_index in x]
-        plt.subplot(3, 1, 2)
-        plt.plot(x, value_predictions)
-        plt.axis([x[0], x[-1], -0.4, 0.4])
-#        plt.xlim(x[0], x[-1])
-        plt.ylabel('value estimate')
+        return fig
 
     colors = ScalarMappable(cmap=plt.get_cmap('hsv')).to_rgba(np.linspace(0, 1, num=36))
 
     def plot_actions():
-        actions = [metadata[other_index]['policy_dist'] for other_index in x]
+        actions = [metadata[index]['policy_dist']]
         actions = np.array(actions).T
         axes = plt.subplot(1, 1, 1, polar=True, axisbg='none')
-        R, theta = np.meshgrid(1 / np.linspace(x.shape[0], 2, num=x.shape[0]+1), np.pi - np.linspace(0, 2*np.pi, num=37) + np.pi/36)
+        R, theta = np.meshgrid([0.8, 0.4], np.pi - np.linspace(0, 2*np.pi, num=37) + np.pi/36)
         plt.ylim(0, 1)
         axes.spines['polar'].set_visible(False)
         plt.xticks([])
@@ -82,21 +90,6 @@ for index in range(2, len(metadata)):
 #        ax.get_yaxis().set_visible(False)
 #        plt.ylabel('snake direction')
 
-#    def plot_diagnostics_time_ranges(prefix, count):
-#        lower_bounds = [
-#            metadata[i]['scalars'].get('diagnostics/' + prefix + '_lb', 0)
-#            for i in x
-#        ]
-#        upper_bounds = [
-#            metadata[i]['scalars'].get('diagnostics/' + prefix + '_ub', 0)
-#            for i in x
-#        ]
-#        plt.subplot(3, 2, count*2)
-#        plt.fill_between(x, lower_bounds, upper_bounds)
-#        plt.xlim(x[0], x[-1])
-#        plt.ylabel(prefix)
-
-
     metadatum = metadata[index]
     screen = screen_from_metadatum(metadatum)
 #    screen = np.expand_dims(screen, axis=-1)
@@ -107,17 +100,41 @@ for index in range(2, len(metadata)):
 
     fig = plt.figure(figsize=screen.shape[:2][::-1], dpi=1)
     fig.patch.set_facecolor('none')
-#    plot_rewards()
-#    plot_value_predictions()
     plot_actions()
-#    plot_diagnostics_time_ranges('observation_lag', 1)
-#    plot_diagnostics_time_ranges('clock_skew', 2)
-#    plot_diagnostics_time_ranges('action_lag', 3)
 
-    overlay = array_from_figure(fig)
+    action_overlay = array_from_figure(fig)
     plt.close(fig)
 
-    alpha = overlay[:,:,1] / 255
+    green = np.array([0, 1, 0])
+    white = np.array([1, 1, 1])
+    red = np.array([1, 0, 0])
+    reward = metadatum['scalars']['reward']
+    value = metadatum['scalars']['value']
+
+    fig = plot_scalar('reward', lambda rel: green * rel + white * (1-rel), 'R', -0.05, 2.1)
+    reward_overlay = array_from_figure(fig)
+    argb = [1, 0, 1, 0]
+    argb_multiplier = np.broadcast_to(argb, reward_overlay.shape)
+    reward_overlay = reward_overlay * argb_multiplier
+    plt.close(fig)
+
+    fig = plot_scalar('value', lambda rel: green * rel + red * (1-rel), 'V', -0.4, 0.4)
+    value_overlay = array_from_figure(fig)
+    plt.close(fig)
+
+    overlay = action_overlay
+    small_graph_height, small_graph_width = reward_overlay.shape[:2]
+    overlay[10:30+small_graph_height*2, 10:20+small_graph_width] = [0.1 * 255, 255, 255, 255]
+    overlay[
+        15:15+small_graph_height,
+        15:15+small_graph_width
+    ] += (reward_overlay * 0.9).astype('uint8')
+    overlay[
+        15+10+small_graph_height:15+10+2*small_graph_height,
+        15:15+small_graph_width
+    ] += (value_overlay * 0.9).astype('uint8')
+
+    alpha = overlay[:,:,1] / 255 # ???
     alpha = alpha * overlay[:,:,0] / 255
     alpha = np.expand_dims(alpha, axis=-1)
     overlay_rgb = overlay[:,:,1:]
